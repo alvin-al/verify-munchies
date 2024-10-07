ebcd420db6979a61bfa44262bda1f555167d41f3
alvvxal <alvvxal@gmail.com>
2024-10-07 05:24:11 +0000

    Add verify page

--- src/verify/[code]/page.jsx
+++ src/verify/[code]/page.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom'; // Import useParams untuk ambil URL params
 import VerifyCodeSlug from '../../components/VerifyCodeSlug';
 
 const VerifyPage = () => {
-  const { code } = useParams(); 
+  const { code } = useParams(); // Ambil slug dari URL
 
+  // Fungsi verify
+  const verify = () => {
+    
+  };
 
+  // Panggil fungsi verify saat komponen di-render
   React.useEffect(() => {
     verify();
   }, [code]);
 
   return (
-    <VerifyCodeSlug code={code} /> 
+    <VerifyCodeSlug code={code} />  // Lempar slug code ke VerifyCodeSlug sebagai prop
   );
 };
 
